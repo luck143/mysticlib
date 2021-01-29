f7e0a824df52c30229295aa5c0d47fda9dfc5243
NeoTron <luck143@gmail.com>
2021-01-29 18:33:54 +0530

    sef

--- index.php
+++ index.php
@@ -1,75 +1,178 @@
 Mysticscripts.net Astrology Apps API Client Demo - For RapidAPI App https://rapidapi.com/mysticscripts/api/astrology5
 <p>Live Demo using this Code base <a href='https://www.mysticscripts.net/rapidapidocs/'>Here</a>
 
+<h1>How to use Mysticscripts API</h1>
+Check this article <a href='https://www.mysticscripts.net/blog/post/how_to_use_mysticscripts_api_/1611841148039/'>Here</a>
+
 
 <h1>How to Use this Code Sample</h1>
+Check this article <a href='https://www.mysticscripts.net/blog/post/how_to_use_github_mysticlib_co/1611845597495/'>Here</a>
+
 <p>Register account in Rapidapi and visit link <a href='https://rapidapi.com/mysticscripts/api/astrology5'>Here</a>
 <br>Subscribe to the API to get the 'RapidAPI Application URL' and 'RapidAPI Key'
 <br>Download the Demo Code Base(PHP scripts) from <a href='https://github.com/luck143/mysticlib'>Here</a>
 <br>Open File mysticlib.php and Edit "$GLOBALS['apihost']" and "$GLOBALS['apikey']" Values with your own RapidAPI Account Details.
 
 
-<h2>API ENDPOINTS Demos</h2>
-<h3>Endpoint - /listapps/astrology/</h3>
-<p>Get List of all Application in 'Astrology' Category (including subcategories)</p>
+<img src="https://i.gyazo.com/f0d9b196a0539cfe5380835170ae8c17.png" alt="..." style="width:100%;" class="litespeed-loading" data-was-processed="true">
+	
+    <p></p>
+    <p>API Endpoints Explainations and Demos - RapidAPI Version</p>
+    <p>
+        <br>
+    </p>
+    <h2>API ENDPOINTS Demos</h2>
+    <h3>Endpoint - /listapps/astrology/</h3>
+    <p>Get List of all Application in 'Astrology' Category (including subcategories)</p>
+    <p>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_o/1611850054307/">click here</a> 
+        <br>
+    </p>
+    <p>Check code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_listapps.php">demo_listapps.php</a>
+    </p>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_listapps.php">Click Here</a>
+    </p>
+    <hr>
+    <h3>Endpoint - /overview/</h3>
+    <p>Get List of all Categories , Sub Categories and Applications in a TREE Format</p>
+    <p>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_l/1611844118504/">click here</a>
+    </p>
+    <p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_overview.php">demo_fetch.php</a>
+    </p>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_overview.php">Click Here</a>
+    </p>
+    <br>
+    <hr>
+    <h3>Endpoint - /fetch/</h3>
+    <br>Get List of all Applications . including Pagination and Searchable by Application Name
+    <br>
+    <br>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/">click here</a>
+    <br>
+    <br>Files associated with this Endpoint:
+    <br>
+    <br>a) <a href="https://github.com/luck143/mysticlib/blob/main/demo_fetch.php">demo_fetch.php</a> 
+    <br>b) <a href="https://github.com/luck143/mysticlib/blob/main/demo_fetch_requirements.php">demo_fetch_requirements.php</a>
+    <br>
+    <br>
+    <br>&nbsp;&nbsp;&nbsp; Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_fetch.php">Click Here</a>
+    <p>
+        <br>
+        <br>
+        <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    </p>
+    <hr>
+    <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    <h3><br></h3>
+    <h3>Endpoint - /details/{application_id}</h3>
+    <p>
+        <br>
+    </p>
+    <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    <p>Get Detail of Application</p>
+    <p>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_d/1611913542723/">click here</a> 
+        <br>
+    </p>
+    <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    <p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_application_details.php">demo_application_details.php</a>
+    </p>
+    <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_d/1611913542723/">Click Here</a>
+    </p>
+    <br>
+    <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_f/1611852958245/"></a>
+    <hr>
+    <br>
+    <h2>Working with Rendered HTML Codes</h2>
+    <br>
+    <h3>Endpoint - /render_form_inputs/</h3>
+    <br>
+    <p>Get All Input Parameters for rendering HTML Form of Application
+        <br>
+    </p>To check this Endpoint Details&nbsp; <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_r/1611922521599/">click here</a>
+    <p></p>
+    <p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_render_form_requirements.php">demo_render_form_requirements.php</a>
+    </p>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_render_form_requirements.php">Click Here</a> 
+        <br>
+    </p>
+    <p>
+        <br>
+        <br>
+        <br>
+    </p>
+    <h3>Endpoint - /render_form/{application_id}</h3>
+    <br>
+    <p>Get Rendered HTML code of Application From and check the rendered result</p>
+    <p>To check this Endpoint Details&nbsp; <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_r/1611920915904/">click here</a>
+    </p>
+    <p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_render_form.php">demo_render_form.php</a> 
+        <br>
+    </p>Live Demo Result of this page - <a href="https://www.mysticscripts.net/rapidapidocs/demo_render_form.php">Click Here</a>
+    <p>
+        <br>
+    </p>
+    <h3>Endpoint - /render_result/{application_id}</h3>
+    <br>
+    <p>Render Application RESULT in HTML CODE :
+        <br>This Endpoint will output the Rendered HTML Code for the application result.
+        <br>Please use your own css File to display this HTML code.</p>
+    <p>To check this Endpoint Details&nbsp; <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_r/1611924014903/">click here <br></a>
+    </p>
+    <p>Check Code in File -&nbsp; demo_render_result.php
+        <br>
+    </p>Live Demo Result of this page - <a href="https://www.mysticscripts.net/rapidapidocs/demo_render_result.php">Click Here</a>
+    <br>
+    <br>
+    <p>
+        <br>
+        <br>
+        <br>
+    </p>
+    <h2>Working with Raw Json Data</h2>
+    <br>
+    <h3>Endpoint - /inputs/{application_id}</h3>
+    <br>
+    <p>Get Input Parameter for Any Single Application.
+        <br>
+    </p>
+    <p>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_i/1611913936592/">click here</a> 
+        <br>
+    </p>
+    <p>File also include library to generate the From HTML Code using the Input Parameters obtain from the API
+        <br>
+    </p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_application_details.php">demo_application_inputs.php</a>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_application_inputs.php">Click Here </a>
+        <br>
+    </p>
+    <p>
+        <br>
+    </p>
+    <h3>Endpoint - /result/{application_id}</h3>
+    <br>
+    <p>Get Application Result:
+        <br>This Endpoint will generate the result of the application based on the inputs you provided. Please make sure youare sending all the required inputs as mentioned in the 'inputs' Api endpoint.The result of this will be in raw JSON Format. you may choose to display this result as you want on your webpage.</p>
+    <p>To check this Endpoint Details <a href="https://www.mysticscripts.net/blog/post/api_endpoint_explaination_r/1611916326595/">click here</a> 
+        <br>
+    </p>Check Code in File - <a href="https://github.com/luck143/mysticlib/blob/main/demo_application_result.php">demo_application_result.php</a>
+    <p>Live Demo Result of this page -<a href="https://www.mysticscripts.net/rapidapidocs/demo_application_result.php">Click Here</a>
+    </p>
+    <p>
+        <br>
+    </p>
+    <p>
+        <br>
+    </p>
+    <p>
+        <br>
+    </p>
+    <p></p>
+    <br>
+    <p>Date : <span class="badge bg-primary">2021-01-29 18:28:22</span>
+    </p>
+    <p>Category : <a class="badge bg-secondary" href="https://www.mysticscripts.net/blog/categories/documentation/">Documentation</a>
+    </p>
+    <p>Tags : <a class="badge bg-success" href="https://www.mysticscripts.net/blog/tags/rapidapi/">Rapidapi</a>
+    </p>
 
-<p>Check code in File - demo_listapps.php</p>
-<p>Result -<a href='demo_listapps.php'>Click Here</a></p>
-<hr>
-
-<h3>Endpoint - /overview/</h3>
-<p>Get List of all Categories , Sub Categories and Applications in a TREE Fromat</p>
-<p>Check Code in File - demo_fetch.php</p>
-<p>Result -<a href='demo_overview.php'>Click Here</a></p><br>
-<hr>
-
-<h3>Endpoint - /fetch/</h3>
-<p>Get List of all Applications . including Pagination and Searchable by Application Name</p>
-<p>Check Code in File - demo_fetch_requirements.php</p>
-<p>For Input Parameters -<a href='demo_fetch_requirements.php'>Click Here</a></p><br>
-<p>Check Code in File - demo_fetch.php</p>
-<p>Result Example -<a href='demo_fetch.php'>Click Here</a></p><br>
-<hr>
-
-<h3>Endpoint - /details/{application_id}</h3>
-<p>Get Detail of Application</p>
-<p>Check Code in File - demo_application_details.php</p>
-<p>Result -<a href='demo_application_details.php'>Click Here</a></p><br>
-<hr>
-
-<h2>Working with Rendered HTML Codes</h2>
-<h3>Endpoint - /render_form_inputs/</h3>
-<p>Get All Input Parameters for rendering HTML Form of Application</p>
-<p>Check Code in File - demo_render_form_requirements.php</p>
-<p>Result -<a href='demo_render_form_requirements.php'>Click Here</a></p><br>
-<hr>
-
-<h3>Endpoint - /details/{application_id}</h3>
-<p>Get Rendered HTML code of Application From and check the rendered result</p>
-<p>For getting the Application Form HTML code and Displaying - Check Code in File - demo_render_form.php</p>
-<p>For getting the Application Result HTML code and Displaying - Check Code in File - demo_render_result.php</p>
-<p>Result -<a href='demo_render_form.php'>Click Here</a></p><br>
-<hr>
-
-<h3>Endpoint - /render_form_inputs/</h3>
-<p>Get All Input Parameters for rendering HTML Form of Application</p>
-<p>Check Code in File - demo_render_form_requirements.php</p>
-<p>Result -<a href='demo_render_form_requirements.php'>Click Here</a></p><br>
-<hr>
-
-
-<h2>Working with Raw Json Data</h2>
-
-<h3>Endpoint - /inputs/{application_id}</h3>
-<p>Get Input Parameter for Any Single Application.
-<br> File also include library to generate the From HTML Code using the Input Parameters obtatin from the API
-</p>
-<p>Check Code in File - demo_application_inputs.php</p>
-<p>Result -<a href='demo_application_inputs.php'>Click Here</a></p>
-
-<p>Result Generation File - demo_application_result.php</p>
-
-<hr>
 
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
